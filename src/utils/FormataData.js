7741c7e2ad319ced19b26132ac8d51d9e5896119
guilherme.araujo14 <guilherme.araujo14@hotmail.com>
2024-03-10 16:20:01 -0300

    Ajuste na tela de importação de planilha, resolução do problema que a navBar estava subindo e escondendo parte, quando uma planilha grande era importada. e também um ajuste na logica de formatar data.

--- src/utils/FormataData.js
+++ src/utils/FormataData.js
@@ -1,8 +1,8 @@
 export default function FormataData(data){
-    data = new Date()
-    let dia = data.getDate()
-    let mes = data.getMonth()
-    let ano = data.getFullYear()
+    let newDate = new Date(data)
+    let dia = newDate.getDate()
+    let mes = newDate.getMonth()+1
+    let ano = newDate.getFullYear()
     let dataFormatada = `${dia}/${mes}/${ano}`
     return dataFormatada
 }
