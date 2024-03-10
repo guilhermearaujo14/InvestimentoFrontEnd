2f1da43e00d5958497193bcc5b8e52bbaf647faf
guilherme.araujo14 <guilherme.araujo14@hotmail.com>
2024-03-10 16:29:54 -0300

    Mais um ajuste para concatenar caso dia e mês sejam menores que 10 concatenar 0 a esquerda da data.

--- src/utils/FormataData.js
+++ src/utils/FormataData.js
@@ -3,6 +3,8 @@ export default function FormataData(data){
     let dia = newDate.getDate()
     let mes = newDate.getMonth()+1
     let ano = newDate.getFullYear()
+    if(dia < 10) dia = "0"+dia
+    if(mes < 10) mes = "0"+mes
     let dataFormatada = `${dia}/${mes}/${ano}`
     return dataFormatada
 }
