ecd4d46aa6ab72f756f73c47e67670d9ea4a12bb
guilherme.araujo14 <guilherme.araujo14@hotmail.com>
2024-03-15 17:28:47 -0300

    Verificação se o valor que esta chegando é undefinid caso seja o valor vai ser atribuído é zero, e assim não deixa de funcionar a aplicação.

--- src/utils/FormataMoeda.js
+++ src/utils/FormataMoeda.js
@@ -1,6 +1,6 @@
 
 function FormataMoeda(valor){
-    if(valor === undefined || valor == 0){
+    if(valor == undefined || valor == 0){
         0
     }else{
         let valorFormatado = valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
