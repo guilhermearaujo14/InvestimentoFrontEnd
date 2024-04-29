ee2a614256f8968760d37f9bc71c68d961ae184d
Guilherme Araujo <guilherme.araujo14@hotmail.com>
2024-04-29 17:18:12 -0300

    Ajuste de caminho

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://high-deadpool-orange-bahrain.bohr.io/api/core/'
+    baseURL: 'https://high-deadpool-orange-bahrain.bohr.io/'
     //http://localhost:3300/'
 })
 export default api;
