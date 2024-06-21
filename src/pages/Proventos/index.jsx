c58e42cac0cf395d63a0b10e0288a5dbe28397f8
Guilherme Araujo <guilherme.araujo14@hotmail.com>
2024-06-21 16:30:42 -0300

    Iniciando tela de proventos

--- src/pages/Proventos/index.jsx
+++ src/pages/Proventos/index.jsx
@@ -1,17 +1,30 @@
-import { useEffect, useState} from 'react'
+import { useState} from 'react'
 import NavBar from '../../components/navbar'
 import './style.css';
 
 import CarregandoDados from '../../components/CarregandoDados';
 import BasicTextField from '../../components/Formulario/TextField';
+import { InputLabel, MenuItem, Select } from '@mui/material';
 
 export default function Proventos(){
-const [mes, setMes] = useState(0)
+const [mes, setMes] = useState()
+const [ano, setAno] = useState()
 
     let isCarregando = false
     
 
-
+const listaMeses = [{id:1, name:'Janeiro'},
+    {id:2, name:'Fevereiro'},
+    {id:3, name:'Março'},
+    {id:4, name:'Abril'},
+    {id:5, name:'Maio'},
+    {id:6, name:'Junho'},
+    {id:7, name:'Julho'},
+    {id:8, name:'Agosto'},
+    {id:9, name:'Setembro'},
+    {id:10, name:'Outubro'},
+    {id:11, name:'Novembro'},
+    {id:12, name:'Dezembro'}]
 
 if(isCarregando){
     return(
@@ -34,8 +47,24 @@ if(isCarregando){
 
                 </div>
                 <div className="container-proventos-table">
-                    <BasicTextField label={"Mês"} name={"mes"} required={"required"} type={"number"} value={mes} onChange={""} />
-                    
+                    <div className='container-mesRef'>
+                        <InputLabel id="MesReferencia" style={{color:'#fff'}}>Mês</InputLabel>
+                        <Select
+                        style={{width: '100%', marginBottom: '10px', color:'#fff', backgroundColor: '#999'}}
+                            labelId='MesReferencia'
+                            value={mes}
+                            onChange={(e)=>setMes(e.target.value)}>
+                            {
+                                listaMeses.map((item)=>{
+                                    return (
+                                        <MenuItem value={item.id} key={item.id}>{item.name}</MenuItem>
+                                )})
+                            }
+                            </Select>
+                    </div>
+
+
+                    <BasicTextField label={"Ano de referência"} name={"ano"} required={"required"} type={"number"} value={ano} onChange={(e)=> setAno(e.target.value)} />
                 </div>
 
             </div>
